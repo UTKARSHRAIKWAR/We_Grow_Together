a6bd5cd6ea97b5a35243d7e547fa06bde2a8fa8d
ranjeet_kumar <ranjeetkumargond000@gmail.com>
2023-08-02 19:44:11 +0530

    final check

---
+++ #c_trainning_stuf/simpleCalc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+    int b = 10;
+
+    printf("%i + %i = %i\n", a, b, a + b);
+    printf("%i - %i = %i\n", a, b, a - b);
+    printf("%i x %i = %i\n", a, b, a * b);
+    printf("%i / %i = %i\n", a, b, a / b);
+    return 0;
+}
